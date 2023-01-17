69ef80981606f4956f6830cf4a3ce39c7f8dd66b
Liam <backgroundpositioncenter@gmail.com>
2023-01-18 01:49:15 +0300

    added combine reducers

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import {createStore, combineReducers} from "redux";
+import {cashReducer} from "./cashReducer";
+import {customReducer} from "./customerReducer";
+
+const rootReducer = combineReducers({
+    cash: cashReducer,
+    customers: customReducer,
+
+})
+
+export const store = createStore(cashReducer)
