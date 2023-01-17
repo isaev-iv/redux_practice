0be70672288eac5e2f0c0afb7a136a9973c01703
Liam <backgroundpositioncenter@gmail.com>
2023-01-18 00:46:10 +0300

    Redux и React. State, reducer, action + redux hooks useDispatch и useSelector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash);
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+
+    }
+
+    return (<div className="App">
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <h1>
+                {cash}
+            </h1>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <button onClick={() => addCash(Number(prompt()))} style={{padding: '10px', fontSize: '20px', background: "lightsteelblue"}}>
+                Пополнить счет
+            </button>
+            <button onClick={() => getCash(Number(prompt()))} style={{padding: '10px', fontSize: '20px', background: "lightsteelblue"}}>
+                Снять со счета
+            </button>
+        </div>
+    </div>);
 }
 
 export default App;
